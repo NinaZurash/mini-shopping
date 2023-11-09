5dad113790f0612ad8931e7b7a5d2c4bb743494f
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-11-09 19:39:09 +0100

    updated colors, added theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8">
       <Button>Welcome!</Button>
-      <ModeToggle className="absolute left-4 top-4" />
+      <ModeToggle className="absolute right-8 top-4" />
     </main>
   );
 }
