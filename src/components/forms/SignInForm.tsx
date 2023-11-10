ceedda8cad38ca1a39eb42559daf545cf653e604
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-11-10 19:15:15 +0100

    add wallpaper

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -31,36 +31,38 @@ export default function ProfileForm() {
   }
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Password</FormLabel>
-              <FormControl>
-                <Input type="password" placeholder="" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <div className="borde rounded-xl border-2 border-indigo-900 bg-gradient-to-r from-sky-500 to-indigo-500 p-14">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
   );
 }
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,8 +1,13 @@
 import SignInForm from "@/components/forms/SignInForm";
 
+const imageUrl =
+  "https://img.freepik.com/premium-vector/contemporary-seamless-pattern-vector-wallpaper_263779-876.jpg?w=2000";
 export default function SignInPage() {
   return (
-    <div className="flex h-screen items-center justify-center">
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="flex h-screen items-center justify-center"
+    >
       <SignInForm />
     </div>
   );
