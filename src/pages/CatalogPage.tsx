bfc9231a220b4ec6bbbf48bbcf451607ede755b8
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-11-15 22:16:07 +0100

    add electronics page

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,17 +1,12 @@
 import { useSearchParams } from "react-router-dom";
 
+import Electronics from "@/components/categories/Electronics";
+
 export default function CatalogPage() {
   const [searchParams] = useSearchParams();
   const categories = searchParams.get("category");
 
-  return (
-    <div className="">
-      {
-        categories
-        // categories === "womans" && <WomansCategory />
-      }
-    </div>
-  );
+  return <div className="">{categories === "electronics" && <Electronics />}</div>;
 }
 
 // S P A  = single page application
