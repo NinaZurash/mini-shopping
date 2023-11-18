de50e864de003037fa6977cd2f3b5ccd2a2865f4
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-11-18 20:56:32 +0100

    update profile page

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDliJsO4IRVlcTocdmD3BaezCqmfOYDXF8",
+  authDomain: "mini-shopping-16778.firebaseapp.com",
+  projectId: "mini-shopping-16778",
+  storageBucket: "mini-shopping-16778.appspot.com",
+  messagingSenderId: "768859662176",
+  appId: "1:768859662176:web:dedd0ebdf923b2cb76e005",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+console.log(app);
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,3 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export default function ProfilePage() {
+  const userSignedIn = false;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userSignedIn) {
+      navigate("/sign-in");
+    }
+  }, [userSignedIn, navigate]);
+
   return <div>ProfilePage</div>;
 }
