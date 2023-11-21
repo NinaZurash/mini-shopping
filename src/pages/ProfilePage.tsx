8c5a13e9ae035ae8452b24139b09e15d5ad23a6e
NinaZurash <nino.zurashvili.nz@gmail.com>
2023-11-21 23:15:22 +0100

    update profile page

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -16,7 +16,7 @@ export default function ProfilePage() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center">
-      <h1 className="mb-4 text-3xl font-bold">Profile </h1>
+      <h1 className="mb-4 text-3xl font-bold">Profile Information</h1>
     </div>
   );
 }
